a46898da7f6ce7026afd64ce392f243e7fb9df0c
David Mora Duque <david.mora02@uceva.edu.co>
2026-09-11 13:26:05 -0500

    feat: agregar avatar atom a public-api

--- projects/design-system-bootstrap/src/public-api.ts
+++ projects/design-system-bootstrap/src/public-api.ts
@@ -11,6 +11,7 @@ export { BadgeAtom } from './lib/presentation/atoms/badge/badge.atom';
 export { ButtonAtom } from './lib/presentation/atoms/button/button.atom';
 export { ContainerAtom } from './lib/presentation/atoms/container/container-atom';
 export { TagAtom } from './lib/presentation/atoms/tag/tag.atom';
+export { AvatarAtom } from './lib/presentation/atoms/avatar/avatar.atom';
 
 //Molecules
 export { NavLinkMolecule } from './lib/presentation/molecules/nav-link/nav-link.molecule';
